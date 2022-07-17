e7aefbe0be47e5b0ce8c536cd6857da1d4e169cf
Cody McCarty <codyjmccarty@gmail.com>
2022-07-16 20:55:46 -0400

    gen online store entity model

---
+++ src/main/java/com/mycompany/store/domain/enumeration/Measurement.java
@@ -0,0 +1,12 @@
+package com.mycompany.store.domain.enumeration;
+
+/**
+ * The Measurement enumeration.
+ */
+public enum Measurement {
+    S,
+    M,
+    L,
+    XL,
+    XXL,
+}
---
+++ src/main/webapp/app/entities/enumerations/measurement.model.ts
@@ -0,0 +1,11 @@
+export enum Measurement {
+  S = 'S',
+
+  M = 'M',
+
+  L = 'L',
+
+  XL = 'XL',
+
+  XXL = 'XXL',
+}
